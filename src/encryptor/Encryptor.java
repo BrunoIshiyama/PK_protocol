f062a313700db06107ede288dfa4e381c99d952e
R1pp3r <r1pp3r@localhost.localdomain>
2018-09-02 10:50:34 -0300

    Encryptor v1 finished

--- src/encryptor/Encryptor.java
+++ src/encryptor/Encryptor.java
@@ -1,6 +1,7 @@
 package encryptor;
 
-import java.math.BigDecimal;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 /*
  * This class is responsible to Encrypt messages 
@@ -9,18 +10,27 @@ import java.math.BigDecimal;
 public class Encryptor {
 	private long publicKeyN;
 	private long publicKeyE;
-	private String msg;
 	
 	private long[] convertMessage(String msg) {
-		return null;
+		long[] convMsg = new long[msg.length()];
+		for(int i = 0;i<convMsg.length;i++) {
+			convMsg[i]= new Integer(msg.charAt(i));
+		}
+		return convMsg;
 	}
 	
-	public long[] encrypt(long[] msg) {
-		long[] eMsg = new long[msg.length];
+	public InputStream encrypt(String msg) {
+		long[] convMsg = convertMessage(msg);
+		long[] eMsg = new long[convMsg.length];
 		for(int i = 0 ; i<eMsg.length ; i++) {
-			eMsg[i] = (long) ((Math.pow((double)msg[i],publicKeyE))%publicKeyN);
+			eMsg[i] = (long) ((Math.pow((double)convMsg[i],publicKeyE))%publicKeyN);
 		}
-		return  null;
+		StringBuilder sendString = new StringBuilder();
+		sendString.append(eMsg[0]);
+		for(int i = 1;i<eMsg.length;i++) {
+			sendString.append(","+eMsg[i]);
+		}
+		return new ByteArrayInputStream(sendString.toString().getBytes());
 	}
 	
 }
